4af7aacbaf0a0c38ee0c1d19a9c70f6ad89c1987
HRK <45808217+HirokoYAMADA@users.noreply.github.com>
2021-04-12 08:07:01 +0900

    update analytics.py

--- plugins/analytics.py
+++ plugins/analytics.py
@@ -2,18 +2,23 @@ import datetime
 import os
 import sys
 import time
+from datetime import timedelta
 
 import pandas as pd
 import requests
+import slack_sdk
 from dotenv import load_dotenv
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from slack_sdk.web import client
 
 
-def hoge():
+def main():
     load_dotenv()
-    print(os.environ)
-    print(os.environ.get('SLACK_TOKEN'))
+    # print(os.environ)
+    # print(os.environ.get('SLACK_TOKEN'))
+    d = datetime.date.today() - timedelta(days=7)
+    print(d)
     client = WebClient(token=os.environ.get('SLACK_TOKEN'))
 
     # Store conversation history
@@ -26,56 +31,51 @@ def hoge():
         # Call the conversations.history method using the WebClient
         # conversations.history returns the first 100 messages by default
         # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
-        result = client.conversations_history(channel=channel_id)
+        result = client.conversations_history(channel=channel_id, oldest=d)
 
         conversation_history = result["messages"]
 
         # Print results
         print("{} messages found in {}".format(len(conversation_history), id))
-        print(conversation_history)
+
+        conversation_history = [
+            {'ts': i['ts'], 'text': i['text'], 'reactions': i['reactions']}
+            for i in conversation_history
+            if 'reactions' in i.keys()
+        ]
+        for i in conversation_history:
+            cnt = 0
+            for s in i['reactions']:
+                cnt += s['count']
+            i['reactions'] = cnt
+
+        return pd.DataFrame(conversation_history)
 
     except SlackApiError as e:
         print("Error creating conversation: {}".format(e))
 
 
-# token = os.environ.get('SLACK_TOKEN')
-# headers = {"Content-type": "application/json", "Authorization": f"Bearer {token}"}
-
-
-#  def fetch_messages_by_channel(channel_id):
-#     oldest_ts = None
-#     one_year_ago = pd.to_datetime('2021-04-04')
-#     endpoint = 'https://slack.com/api/channels.history'
-
-#     ls_messages = []
-#     while True:
-#         payload = {'channel': channel_id, 'latest': oldest_ts, 'count': 1000}
-
-#         data = requests.get(endpoint, headers=headers, params=payload).json()
-#         print(data)
-#         messages = data['messages']
-#         ls_messages.extend(messages)
-
-#         if data['has_more']:
-#             time.sleep(1)
-#             oldest_ts = messages[-1]['ts']
-#             oldest_datetime = pd.to_datetime(oldest_ts, unit='s')
-#             sys.stdout.write(f"\r{oldest_datetime}")
-#             sys.stdout.flush()
-#             if oldest_datetime < one_year_ago:
-#                 sys.stdout.write(f"\rfinish!" + ' ' * 50)
-#                 break
-#         else:
-#             break
-#     df = pd.DataFrame(ls_messages)
-#     df['channel_id'] = channel_id
-#     return df
-
-
-# def main():
-#     print(fetch_messages_by_channel('C01SGT4J2AC'))
-
-
 if __name__ == '__main__':
-    print(os.environ)
-    # hoge()
+    conversation_history = main()
+    print(pd.to_datetime(conversation_history['ts'], unit='s'))
+
+    df_best_comments = conversation_history[
+        conversation_history.reactions == conversation_history.reactions.max()
+    ]
+    # print(best_comments)
+
+    best_comments = ''
+    for _, i in df_best_comments.iterrows():
+        client = WebClient(token=os.environ.get('SLACK_TOKEN'))
+        # print(i)
+        chat = client.chat_getPermalink(
+            token=os.environ.get('SLACK_TOKEN'),
+            channel=os.environ.get('CHANNEL_ID'),
+            message_ts=i['ts'],
+        )
+        best_comments = '今週のベスト褒めエピソードはこれや:rose:' + chat['permalink']
+        # response = client.chat_postMessage(channel=chat['channel'], text=best_comments)
+
+    print(best_comments)
+
+    # print(chat)
