26182ea15da8df0b7e0d75d6fb199f525a4f688b
Ayana2020 <m.cha.imp.0623@gmail.com>
2021-04-19 09:24:40 +0900

    不要コメントの削除、スレッド内返信機能のリファクタ

--- plugins/botmodule.py
+++ plugins/botmodule.py
@@ -21,35 +21,13 @@ def validation_bot_subtype(message):
     return False
 
 
-# @listen_to(r'.*@.*@.*')
-# def divede_mention(message):
-#     """ 複数メンション時にメンションを個々に分けてメッセージを投下 """
-
-#     if validation_bot_subtype(message):
-#         return None
-
-#     message = add_bot_message_subtype(message)
-
-#     # メンション(@hoge.hoge など) は slackID(<@〇〇>)に自動で変換される
-#     m = re.compile(r'<@.*>')
-#     text = message.body['text']
-#     # コピペした場合は' ',スラックでメンションを連続して入力する場合には'\xa0'が引っかかる
-#     text_list = re.split('[\xa0| |,|;]', text)
-#     # print(text_list)
-#     for i in text_list:
-#         mo = m.match(i)
-#         if mo is not None:
-#             mnsmsg = mo.group()
-#             message.send(mnsmsg)
-
-
 @listen_to(r'.*@.*')
 def homeru_post(message):
     """
     メンション付きの投稿がされた場合に、メッセージ内のメンションされた人をほめる機能
     """
 
-    # TODO: ありがとう系の投稿の場合、
+    # TODO: validationする
 
     homeru_message_list = [
         'のことほんま尊敬するわ:star:',
@@ -60,14 +38,11 @@ def homeru_post(message):
         'お前がおらんかったら無理やったで:Hello:',
     ]
 
-    #
-
-    # TODO:メンションされたユーザー名を取り出す
     text = message.body['text']
     print(text)
 
     m = re.compile(r'<@.*>')
-
+    # TODO: コピペした場合は' ',スラックでメンションを連続して入力する場合には'\xa0'が引っかかる
     text_list = re.split('[\xa0| |,|;]', text)
 
     for i in text_list:
@@ -82,33 +57,9 @@ def homeru_post(message):
 
             mnsmsg = mo.group()
 
-            # スレッド内の返信には、スレッド内でからむ仕様にする
-            try:
+            # TODO:スレッド内のユーザーの返信に、スレッドの外で反応すると会話の流れがわかりにくいため
+            if 'thread_ts' in message.body:
                 message.send(f'{mnsmsg} {homeru_message}', thread_ts=message.body['thread_ts'])
 
-            except:
+            else:
                 message.send(f'{mnsmsg} {homeru_message}')
-
-    # TODO: ほめる君自身の投稿には反応しない仕様にする→# 知らない言葉を聞いた時のデフォルトの応答で対応
-
-    # TODO: スレッド内の投稿を拾うかを決める(デフォルトはチャネル内返信)
-
-    # TODO: 拾う場合：スレッド内でからむ
-
-    # TODO: 拾わない場合：拾わない仕様にする
-
-    # ほめる言葉を作成する
-    # TODO:ほめる言葉＋アイコンにする
-
-    # homeru_message_list = (
-    #     'のことほんま尊敬するわ:star:',
-    #     '相変わらずすごいやつやな:rose:',
-    #     'ほんとめっちゃ助かってるで',
-    #     'いつもさんきゅーやで',
-    # )
-    # homeru_message = random.choice(homeru_message_list)
-
-    # # TODO: メンション付きのほめる言葉を作成
-
-    # # ほめる言葉を送信する
-    # message.send(homeru_message)
