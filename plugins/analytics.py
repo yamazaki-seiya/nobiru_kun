ee6e447bf1a645acca68037ee0df61298b10c13c
Hiroko.YAMADA <45808217+HirokoYAMADA@users.noreply.github.com>
2021-04-26 00:30:56 +0900

    週次集計と最褒めエピソードのチャンネル投稿のための機能実装

--- plugins/analytics.py
+++ plugins/analytics.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import re
 from datetime import timedelta
 
 import pandas as pd
@@ -22,7 +23,7 @@ def main():
     load_dotenv()
     # print(os.environ)
     # print(os.environ.get('SLACK_TOKEN'))
-    d = datetime.date.today() - timedelta(days=7)
+    d = datetime.datetime.now() - timedelta(days=7)
     print(d)
     client = WebClient(token=os.environ.get('SLACK_TOKEN'))
 
@@ -37,7 +38,9 @@ def main():
         # conversations.history returns the first 100 messages by default
         # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination # noqa: E501
 
-        result = client.conversations_history(channel=channel_id, oldest=d)
+        result = client.conversations_history(
+            channel=channel_id, oldest=d.timestamp(), limit=100000
+        )
 
         conversation_history = result['messages']
 
@@ -45,10 +48,12 @@ def main():
         print('{} messages found in {}'.format(len(conversation_history), id))
 
         conversation_history = [
-            {'ts': i['ts'], 'text': i['text'], 'reactions': i['reactions']}
+            {'ts': i['ts'], 'text': i['text'], 'reactions': i['reactions'], 'user': i['user']}
             for i in conversation_history
             if 'reactions' in i.keys()
         ]
+
+        # print(conversation_history)
 
         for i in conversation_history:
             cnt = 0
@@ -60,22 +65,48 @@ def main():
 
         df_best_comments = conversation_history[
             conversation_history.reactions == conversation_history.reactions.max()
-        ]
-        # print(best_comments)
+        ].reset_index()
+        # print(df_best_comments)
 
-        best_comments = ''
-        for _, i in df_best_comments.iterrows():
+        for idx, r in df_best_comments.iterrows():
             client = WebClient(token=os.environ.get('SLACK_TOKEN'))
-            # print(i)
+
             chat = client.chat_getPermalink(
                 token=os.environ.get('SLACK_TOKEN'),
                 channel=channel_id,
-                message_ts=i['ts'],
+                message_ts=r['ts'],
             )
-            best_comments = '今週のベスト褒めエピソードはこれや:rose:' + chat['permalink']
-            # response = client.chat_postMessage(channel=chat['channel'], text=best_comments)
+            # print(idx)
+            # print(r['text'])
 
-        print(best_comments)
+            m = re.compile(r'<@.*>')
+            # コピペした場合は' ',スラックでメンションを連続して入力する場合には'\xa0'が引っかかる
+            text_list = re.split('[\xa0| |,|;]', r['text'])
+            # print(text_list)
+
+            homember_list = [m.match(name).group() for name in text_list]  # if m.match(name) !=
+            # print(homember_list)
+
+            if idx == 0:
+                response = client.chat_postMessage(
+                    channel=chat['channel'],
+                    text='先週もようがんばったな:kissing_cat:ノビルくんの弟からウィークリーレポートのお知らせやで～\n'
+                    + '先週みんなが送ってくれた「褒め言葉」の中で、一番多くのスタンプを集めたウィークリーベスト褒めエピソードはこれや！:cv2_res_pect:\n',
+                )
+
+            response = client.chat_postMessage(
+                channel=chat['channel'],
+                text=f'最もリアクションの多かった褒めをした人：<@{r["user"]}>\n'
+                + f'最も褒められたメンバー：{", ".join(homember_list)}\n',
+            )
+
+            response = client.chat_postMessage(
+                channel=chat['channel'], text=f'{chat["permalink"]}\n'
+            )
+
+        response = client.chat_postMessage(  # noqa: F841
+            channel=chat['channel'], text='今週もぎょうさん褒めに褒めまくって、伸ばし合っていこか！'
+        )
 
     except SlackApiError as e:
         print('Error creating conversation: {}'.format(e))
