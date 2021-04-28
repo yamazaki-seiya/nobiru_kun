91b265c3640013ef5416058c9ff9f388a8c3ae48
seiya.yamazaki <seiya5200128@gmail.com>
2021-04-28 21:57:53 +0900

    [fix] function comments

--- plugins/botmodule.py
+++ plugins/botmodule.py
@@ -14,13 +14,13 @@ _MESSAGE_SPLIT_PATTERN = re.compile(r'[\xa0| |,|;]')
 
 
 def add_bot_message_subtype(message):
-    """ボットのメッセージだとわかるように判別をつける """
+    """ボットのメッセージだとわかるように判別をつける"""
     message.body['subtype'] = 'bot_message'
     return message
 
 
 def validation_bot_subtype(message):
-    """ボットのメッセージか判定する """
+    """ボットのメッセージか判定する"""
     if 'subtype' in message.body and message.body['subtype'] == 'bot_message':
         return True
     return False
