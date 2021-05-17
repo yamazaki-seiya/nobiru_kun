330aa03f8c484715a659d30c66265b2e909a89c1
Hiroko.YAMADA <45808217+HirokoYAMADA@users.noreply.github.com>
2021-05-17 12:24:52 +0900

    typehintのdictをtyping.Dictに変更

--- analytics.py
+++ analytics.py
@@ -3,7 +3,7 @@ import inspect
 import os
 import re
 from datetime import timedelta
-from typing import List
+from typing import Dict, List
 
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -30,7 +30,7 @@ def post_award_best_home_weekly() -> None:
         print('Error creating conversation: {}'.format(e))
 
 
-def _extract_most_reacted_posts(trace_back_days: int) -> List[dict]:
+def _extract_most_reacted_posts(trace_back_days: int) -> List[Dict]:
     """リアクション付き投稿リストのうちで最もリアクション数の多かった投稿を抽出する"""
     posts_with_reaction = _get_posts_with_reaction(trace_back_days)
     max_reaction_cnt = max([post['reactions_cnt'] for post in posts_with_reaction])
@@ -40,7 +40,7 @@ def _extract_most_reacted_posts(trace_back_days: int) -> List[dict]:
     return most_reacted_posts
 
 
-def _get_posts_with_reaction(trace_back_days: int) -> List[dict]:
+def _get_posts_with_reaction(trace_back_days: int) -> List[Dict]:
     """指定された日数遡った期間のリアクション付き投稿（botからの投稿は除く）を1投稿1辞書型のリストとして取得する"""
 
     oldest_day = datetime.datetime.now() - timedelta(days=trace_back_days)
@@ -79,7 +79,7 @@ def _post_start_message() -> None:
     _post_message(message)
 
 
-def _post_award_message(post: dict) -> None:
+def _post_award_message(post: Dict) -> None:
     """最もリアクションが多かった投稿をしたユーザ、メンションされたユーザ、投稿へのリンクを投稿する"""
     chat_link = _get_post_link(post['ts'])
     homember_list = _get_homember_list(post['text'])
