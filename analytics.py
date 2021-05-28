dc88b2a35c6a78c41c817d370edbc58dcedc4ccb
HRK <45808217+HirokoYAMADA@users.noreply.github.com>
2021-05-28 17:05:40 +0900

    週次でもっともリアクションが多かった投稿を取得して再投稿する機能の実装 (#22)
    * create new .py file for slack comments analysis
    
    * Update requirements.txt
    
    add modules to import
    
    * update analytics.py
    
    * fix linter point
    
    * 週次集計と最褒めエピソードのチャンネル投稿のための機能実装
    
    * プリント文の削除と変数名の変更
    
    * TODOを変更
    
    * 不要な変数を削除
    
    * delete sympy
    
    * 変数名をわかりやすく変更、コメントを追加
    
    * 環境変数として.envを読み込むように追記
    
    * 機能はそのままにメソッドごとに切り分けを実施しました。
    
    * 一部の関数を外部から参照できないように変更。
    
    * slacksdkの後のignoreを削除、引数・返り値に型ヒントを記載
    
    * 変数名がdとなっているところを具体的な名称に変更
    
    * リアクションされた投稿を抽出すると同時にリアクションの総数を算出するように変更
    
    * _get_post_linkのコメントから型名を削除（引数の方に型ヒントは記載済み）
    
    * _get_post_linkの返り値をpermalinkに変更
    
    * slack_sdkのバージョンを最新のものに変更、python_dotenvを削除
    
    * trace_back_daysのデフォルト値を削除
    
    * 投稿内容のみを指定すれば投稿できる関数_post_messageを作成
    
    * 投稿コメントに当たる文字列の表記方法を変更
    
    * messageのダブルクオーテーションを無視するコメントを追加
    
    * もろもろの修正（関数の順序・w→with・inspct.cleandocの実行位置）
    
    * trace_back_daysをglobal変数として宣言するようにした。
    
    * TRACE_BACK_DAYSを_TRACE_BACK_DAYSに修正
    
    * メンションされているユーザのリスト取得のための正規表現を変更
    
    * ユーザリスト取得のための正規表現をglobal関数に記述
    
    * botからの投稿を無視するようにした。
    
    * _get_posts_with_reactionのコメントにbotからの投稿を除外している旨を追記
    
    * _post_messageのdockstringに変数messageに関する注意点を記載
    
    * 型ヒントlistの中身の記載、typing.Listのimport
    
    * ファイルの場所をplugin配下からhomru_bot配下に移動
    
    * コメント内のタイポ修正
    
    * 型ヒントlistで中身の指定が抜けていた部分を指定
    
    * _get_posts_with_reactionのコメントを修正
    
    * pprintの削除（printへの書き換え）
    
    * 変数messageで使われているクオーテーションをシングルに修正。
    
    * typehintのdictをtyping.Dictに変更
    
    * より明確にするため_get_post_linkのコメントのtsをtimestampに書き換えた
    
    * _post_messageのmessageを三連引用符からシングルクオーテーション一つに変更
    
    * 不要な空のlistの作成を削除

---
+++ analytics.py
@@ -0,0 +1,127 @@
+import datetime
+import inspect
+import os
+import re
+from datetime import timedelta
+from typing import Dict, List
+
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+SLACK_TOKEN = os.environ['SLACK_TOKEN']
+CHANNEL_ID = os.environ['CHANNEL_ID']
+CLIENT = WebClient(token=SLACK_TOKEN)
+_TRACE_BACK_DAYS = 7
+_EXTRACT_USER_PATTERN = re.compile(r'<@\w*>')
+
+
+def post_award_best_home_weekly() -> None:
+    """実行日から過去7日間の投稿を取得し最もリアクションの多かった投稿を表彰する"""
+    try:
+        most_reacted_posts = _extract_most_reacted_posts(_TRACE_BACK_DAYS)
+        _post_start_message()
+
+        for post in most_reacted_posts:
+            _post_award_message(post)
+
+        _post_end_message()
+
+    except SlackApiError as e:
+        print('Error creating conversation: {}'.format(e))
+
+
+def _extract_most_reacted_posts(trace_back_days: int) -> List[Dict]:
+    """リアクション付き投稿リストのうちで最もリアクション数の多かった投稿を抽出する"""
+    posts_with_reaction = _get_posts_with_reaction(trace_back_days)
+    max_reaction_cnt = max([post['reactions_cnt'] for post in posts_with_reaction])
+    most_reacted_posts = [
+        post for post in posts_with_reaction if post['reactions_cnt'] == max_reaction_cnt
+    ]
+    return most_reacted_posts
+
+
+def _get_posts_with_reaction(trace_back_days: int) -> List[Dict]:
+    """指定された日数遡った期間のリアクション付き投稿（botからの投稿は除く）を1投稿1辞書型のリストとして取得する"""
+
+    oldest_day = datetime.datetime.now() - timedelta(days=trace_back_days)
+    # extracted_posts = []
+
+    # 実行日からtrace_back_days日前までの投稿を取得
+    result = CLIENT.conversations_history(
+        channel=CHANNEL_ID, oldest=oldest_day.timestamp(), limit=100000
+    )
+    extracted_posts = result['messages']
+    print(f'{len(extracted_posts)} messages found')
+
+    # リアクションされた投稿のみを抽出
+    extracted_posts_with_reaction = [
+        {
+            'ts': post['ts'],
+            'text': post['text'],
+            'reactions_cnt': sum(reaction['count'] for reaction in post['reactions']),
+            'user': post['user'],
+        }
+        for post in extracted_posts
+        if ('bot_id' not in post.keys())
+        & ('reactions' in post.keys())  # botからの投稿とreactionのない投稿を除外する
+    ]
+    print(f'extracted_posts_with_reaction:\n{extracted_posts_with_reaction}')
+
+    return extracted_posts_with_reaction
+
+
+def _post_start_message() -> None:
+    """レポート最初のコメントを投稿する"""
+    message = '''
+        先週もようがんばったな:kissing_cat:ノビルくんの弟からウィークリーレポートのお知らせやで～
+        先週みんなが送ってくれた「褒め言葉」の中で、一番多くのスタンプを集めたウィークリーベスト褒めエピソードはこれや！:cv2_res_pect:
+    '''
+    _post_message(message)
+
+
+def _post_award_message(post: Dict) -> None:
+    """最もリアクションが多かった投稿をしたユーザ、メンションされたユーザ、投稿へのリンクを投稿する"""
+    chat_link = _get_post_link(post['ts'])
+    homember_list = _get_homember_list(post['text'])
+    message = f'''
+        最もリアクションの多かった褒めをした人：<@{post['user']}>
+        最も褒められたメンバー：{', '.join(homember_list)}
+        {chat_link}
+    '''
+    _post_message(message)
+
+
+def _get_post_link(ts: str) -> str:
+    """timestampの一致する投稿のリンクを取得する"""
+    chat = CLIENT.chat_getPermalink(token=SLACK_TOKEN, channel=CHANNEL_ID, message_ts=ts)
+    return chat['permalink']
+
+
+def _get_homember_list(message: str) -> List[str]:
+    """投稿内でメンションされているユーザのリストを取得"""
+    homember_list = re.findall(_EXTRACT_USER_PATTERN, message)
+    print(homember_list)
+    return homember_list
+
+
+def _post_end_message() -> None:
+    """レポートを締めるコメントを投稿する"""
+    message = '今週もぎょうさん褒めに褒めまくって、伸ばし合っていこか！'
+    _post_message(message)
+
+
+def _post_message(message: str) -> None:
+    """
+    CHANNEL_IDのチャンネルにメッセージを送信する
+
+    Args:
+        message: 送信メッセージ
+        メッセージの前後はtrimされる
+        その他空行やタブ文字などがあると変換されるため注意
+        https://docs.python.org/ja/3/library/inspect.html#inspect.cleandoc
+    """
+    CLIENT.chat_postMessage(channel=CHANNEL_ID, text=inspect.cleandoc(message))
+
+
+if __name__ == '__main__':
+    post_award_best_home_weekly()
