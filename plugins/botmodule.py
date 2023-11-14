7d53246fcb5f37b8e2cd357227291f2f47866e26
nojiri-arise <masane.nojiri@ariseanalytics.com>
2023-11-14 12:04:21 +0900

    slack_boltで動作するようにした

--- plugins/botmodule.py
+++ plugins/botmodule.py
@@ -2,22 +2,21 @@ import csv
 import math
 import random
 import re
-
-from slackbot.bot import listen_to
+from typing import Any
+from slack_bolt import Say
 
 # Slackへの投稿コメントからユーザーIDを抽出するための正規表現パターン
 _EXTRACT_USER_PATTERN = re.compile(r'(<@\w*>|<\!subteam\^.*>)')
 
 
-@listen_to(r'.*@.*')
-def homeru_post(message):
+def homeru_post(message: dict[str, Any], say: Say):
     """
     メンション付きの投稿がされた場合に、メッセージ内のメンションされた人をほめる機能
     """
-    if _is_bot_message(message):
+    if 'user' not in message:
         # botからのメッセージの場合反応しない
         return
-    text = message.body['text']
+    text = message['text']
     print(f'ポストされたメッセージ: {text}')
     user_list = _extract_users(text)
     print(f'user_num: {len(user_list)}')
@@ -25,22 +24,10 @@ def homeru_post(message):
     post_message = _get_post_message(user_list)
 
     # スレッド内のユーザーの返信に、スレッドの外で反応すると会話の流れがわかりにくいため
-    message.send(
+    say(
         post_message,
-        thread_ts=message.body['thread_ts'] if 'thread_ts' in message.body else None,
+        thread_ts=message['thread_ts'] if 'thread_ts' in message else None,
     )
-
-
-def _is_bot_message(message):
-    """ボットからのメッセージかを判定する"""
-    hubot_flag = ('subtype' in message.body) and (message.body['subtype'] == 'bot_message')
-    auto_reply_flag = ('subtype' in message.body) and (message.body['subtype'] == 'channel_join')
-    slack_bot_flag = 'bot_id' in message.body
-
-    result = any((hubot_flag, auto_reply_flag, slack_bot_flag))
-
-    print(f'is bot message: {result}')
-    return result
 
 
 def _create_random_element_list(path, user_num):
